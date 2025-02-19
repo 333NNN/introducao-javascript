dc583763b0b9b8c0d75a74f6be4cf1a96e8be18d
333NNN <nata.cunha@aluno.senai.br>
2025-02-19 19:53:25 -0300

    DOM

---
+++ js/arquivo.js
@@ -0,0 +1,8 @@
+var p = document.getElementById("id_p");
+console.log(p);
+
+window.onload = function () {
+  var p = document.getElementById("id_p");
+  console.log(p);
+  p.style.backgroundColor = "#0087FF";
+};
