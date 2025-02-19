a754ff6b292032c3eed12748306960ad92c042e3
333NNN <nata.cunha@aluno.senai.br>
2025-02-19 20:16:41 -0300

    funções

--- js/arquivo.js
+++ js/arquivo.js
@@ -5,4 +5,12 @@ window.onload = function () {
   var p = document.getElementById("id_p");
   console.log(p);
   p.style.backgroundColor = "#0087FF";
+
+  var links = p.getElementsByTagName("a");
+
+  alert(links[0].href);
 };
+
+// myFunc () {
+  
+// }
